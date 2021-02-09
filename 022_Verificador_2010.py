e8bfa9ea1989a480b8af03191ce5f8d8f1a62232
Fvalenci2020 <fvalenci@gmail.com>
2021-02-09 13:06:29 -0300

    correccion

--- 022_Verificador_2010.py
+++ 022_Verificador_2010.py
@@ -9,14 +9,12 @@ from P015_PROC_Verificador import *
 
     #Datos Computador Fco
 ConectorDB='Driver={SQL Server};Server=GTD-NOT019\SQLSERVER2012;Database=PNP_2;Trusted_Connection=yes;'
-
     #Datos Computador Carla
 #ConectorDB='Driver={SQL Server};Server=Server=DESKTOP-SSPJTJO\SQLEXPRESS;Database=Modelo PNP;Trusted_Connection=yes;'
 
     #Definición de Versión
-VersionDescripcion='2011V1'#DEFINIR DESCRIPCIÓN DE VERSIÓN
-
         #Agrega versión a base de datos
+            #VersionDescripcion='2011V1'#DEFINIR DESCRIPCIÓN DE VERSIÓN
             #import pyodbc
             #conn = pyodbc.connect(ConectorDB)
             #cursor = conn.cursor()
@@ -24,14 +22,22 @@ VersionDescripcion='2011V1'#DEFINIR DESCRIPCIÓN DE VERSIÓN
             #conn.commit()
             #IdVersion=pd.read_sql_query("select idversion from versionefact  where Descripcion='"+VersionDescripcion+"'",conn)
 
+#Para diciembre  2020
 IdVersion=22
 
     #IMPORTAR DATOS
 path = 'C:/fvalenci/CNE/PNP/PNP_2007_11-12_FPEC/Input/CEN/Entrega_Revisión_EFacDx_2012.v01.xlsx'
 
 Efact_error,Efact=Validador(ConectorDB,path,IdVersion)#
-
 Efact_corregido=CorrectorEfact(ConectorDB,IdVersion,Efact)#
-
 CargaEfactDB(ConectorDB,Efact_corregido)#
 
+#Para noviembre  2020
+IdVersion=20
+
+    #IMPORTAR DATOS
+path = 'C:/fvalenci/CNE/PNP/PNP_2007_11-12_FPEC/Input/CEN/Entrega_Revisión_EFacDx_2011.v01.xlsx'
+
+Efact_error,Efact=Validador(ConectorDB,path,IdVersion)#
+Efact_corregido=CorrectorEfact(ConectorDB,IdVersion,Efact)#
+CargaEfactDB(ConectorDB,Efact_corregido)#
